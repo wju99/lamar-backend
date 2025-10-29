c24dd17f73c645c86c3368c57e845ca9aba67a6f
Will Ju <70983229+wju99@users.noreply.github.com>
2025-10-29 19:51:20 -0400

    fixing cors issue

--- lamar_backend/settings.py
+++ lamar_backend/settings.py
@@ -141,6 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://lamar-frontend-app.vercel.app",
 ]
 
 # Allow credentials (cookies, authorization headers, etc.)
